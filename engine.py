c0f9789090a8b5cc7b897a3fc439cb81fcfd9f2d
Ross Rheingans-Yoo <ross@poker.camp>
2024-07-21 23:27:01 +0000

    101 card -> 100 card

--- engine.py
+++ engine.py
@@ -26,7 +26,7 @@ random.seed(68127)
 
 class KuhnDeck:
     def __init__(self):
-        self.cards = list(range(101))
+        self.cards = list(range(100))
         random.shuffle(self.cards)
         self.index = 0
     
